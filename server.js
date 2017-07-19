237105c254de92880c330bb5f75d9564d7454234
1moosey1 <jgonzalez498@gmail.com>
2017-07-19 01:07:02 -0700

    Comleted timestamp microservice project

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/(:date)?", function(request, response) {
+
+    if(request.params) {
+
+        var rawDate = request.params.date, date;
+        if(time = parseInt(rawDate))
+            date = new Date(time*1000);
+        else
+            date = new Date(rawDate);
+
+        response.send(JSON.stringify({
+
+            unix: date.getTime()/1000,
+            natural: date.toDateString()
+        }));
+    }
+});
+
+app.listen(1337);
